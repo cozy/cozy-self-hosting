dedf2201083a674967555412847a5beb4de60d32
Nicolas Ledez <github@ledez.net>
2015-11-25 11:29:30 +0100

    Update paths

--- server/controllers/debian.js
+++ server/controllers/debian.js
@@ -33,7 +33,7 @@ function update_fqdn(req, res) {
         res.status(400).send({ message: "missing parameters" });
     } else {
         // Exec reconfigure of package
-        child = exec('sudo /usr/local/sbin/debian-reconfigure-cozy-domain.sh "' + params.fqdn + '" > /tmp/debian-reconfigure-cozy-domain.txt', function (error, stdout, stderr) {
+        child = exec('sudo /usr/share/cozy/debian-reconfigure-cozy-domain.sh "' + params.fqdn + '" > /tmp/debian-reconfigure-cozy-domain.txt', function (error, stdout, stderr) {
             console.log('stdout: ' + stdout);
             console.log('stderr: ' + stderr);
             if (error !== null) {
