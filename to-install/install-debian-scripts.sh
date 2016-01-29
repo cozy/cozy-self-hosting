3046110e8342599bd0630332f62a7a42b3d32afd
Nicolas Ledez <github@ledez.net>
2016-01-29 13:30:07 +0100

    Add config file

--- to-install/install-debian-scripts.sh
+++ to-install/install-debian-scripts.sh
@@ -6,6 +6,10 @@ cozy-monitor install ${APP_NAME} -r https://github.com/cozy/cozy-self-hosting.gi
 
 cp ${SOURCE_DIR}/debian-reconfigure-cozy-domain.sh /usr/local/sbin/debian-reconfigure-cozy-domain.sh
 cp ${SOURCE_DIR}/sudoers.d_cozy-self-hosting /etc/sudoers.d/cozy-self-hosting
+cp ${SOURCE_DIR}/self-hosting.json /etc/cozy/self-hosting.json
 
 chown root:root /etc/sudoers.d/cozy-self-hosting
 chmod 440 /etc/sudoers.d/cozy-self-hosting
+
+chown root:root /etc/cozy/self-hosting.json
+chmod 644 /etc/cozy/self-hosting.json
