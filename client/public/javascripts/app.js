d6c43302c205e1b6cf8d84d4c39c64c5a72a7b64
someaveragedev <zoulou31@free.fr>
2016-05-10 20:19:50 +0200

    removing app title on client

--- client/public/javascripts/app.js
+++ client/public/javascripts/app.js
@@ -199,6 +199,8 @@ var show_halt_reboot_buttons = function() {
 var database_maintenance = function(option) {
 	var url = "./debian/database/"+option;
 
+	$("#div-status-ok").html('Database maintenance operation "' + option + '" is running...').show();
+
     $.get(url, function(data) {
         $("#div-status-ko").hide();
         $("#div-status-ok").html(data.message).show();
